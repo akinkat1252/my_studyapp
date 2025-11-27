b33b4ffb062745accafc2b955a1ab61fab3670c3
akinkat1252 <akinkat1531@gmail.com>
2025-11-27 23:41:31 +0900

    add: css file

--- task_management/views.py
+++ task_management/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View, generic
+
 
 # Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = 'task_management/index.html'
+
