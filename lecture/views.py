283360306ec54ec309fd8478ae57aeb79179b7b7
akinkat1252 <akinkat1531@gmail.com>
2026-01-01 23:59:21 +0900

    Fix: Changed the display of Lecture Outline, changed to associate Outline with Subtopic

--- lecture/views.py
+++ lecture/views.py
@@ -1,13 +1,14 @@
 import json
 import markdown
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import IntegrityError, transaction
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from django.utils.safestring import mark_safe
 
 from task_management.models import LearningMainTopic, LearningSubTopic
-from .models import LectureSession, LectureLog
+from .models import LectureSession, LectureLog, LectureTopic
 from .services import create_new_lecture_session
 from ai_support.modules.lecture.generate_lecture import generate_lecture_outline
 
@@ -22,17 +23,33 @@ class LectureView(LoginRequiredMixin, View):
             user=self.request.user,
             id=topic_id,
         )
-
+        # Get previous session to reuse "lecture topic"
+        outlines_qs = sub_topic.lecture_topics.all()
+        # Create new session
         session = create_new_lecture_session(user=request.user, sub_topic=sub_topic)
-        # Generate lecture outline (response(AIMessage) -> )
-        response = generate_lecture_outline(session=session)
-        generated_outline = json.loads(response.content)
 
-        md_lines = []
-        for item in generated_outline:
-            md_lines.append(f"{item['order']}. {item['title']}")
+        if not outlines_qs.exists():
+            # Generate lecture outline (response(AIMessage) -> )
+            response = generate_lecture_outline(session=session)
+            generated_outline = json.loads(response.content)
 
-        md_text = "\n".join(md_lines)
+            try:
+                with transaction.atomic():
+                    if not LectureTopic.objects.filter(sub_topic=sub_topic).exists():
+                        for item in generated_outline:
+                            LectureTopic.objects.create(
+                                sub_topic=sub_topic,
+                                order=item['order'],
+                                title=item['title'],
+                            )
+            except IntegrityError:
+                pass
+
+            outlines_qs = sub_topic.lecture_topics.all()
+
+        md_text = "\n".join(
+            f"{outline.order}. {outline.title}" for outline in outlines_qs
+        )
 
         context = {
             "session": session,
