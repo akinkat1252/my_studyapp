8ba6c61f7af64e104639e1fc66bb5e5257fe87a5
akinkat1252 <akinkat1531@gmail.com>
2026-01-02 20:38:07 +0900

    Create: LectureProgress model

--- lecture/urls.py
+++ lecture/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'lecture'
 urlpatterns = [
-    path('<int:topic_id>/', views.LectureView.as_view(), name='lecture'),
+    path('<int:topic_id>/', views.LectureStartView.as_view(), name='lecture'),
 ]
